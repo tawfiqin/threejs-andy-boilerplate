5b362de24c03f613cae425c0fb3a7ea0d4515211
Tawfiq abohalawah <tawfiqin@gmail.com>
2024-04-01 17:25:10 +0300

    3d scene

--- src/script.js
+++ src/script.js
@@ -6,6 +6,8 @@ import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 /////////////////////////////////////////////////////////////////////////
 //// DRACO LOADER TO LOAD DRACO COMPRESSED MODELS FROM BLENDER
@@ -31,12 +33,15 @@ const renderer = new THREE.WebGLRenderer({ antialias: true}) // turn on antialia
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) //set pixel ratio
 renderer.setSize(window.innerWidth, window.innerHeight) // make it full screen
 renderer.outputEncoding = THREE.sRGBEncoding // set color encoding
+renderer.shadows = true;
+//renderer.shadowType = 1;
+renderer.shadowMap.enabled = true;
 container.appendChild(renderer.domElement) // add the renderer to html div
 
 /////////////////////////////////////////////////////////////////////////
 ///// CAMERAS CONFIG
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
-camera.position.set(34,16,-20)
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 2000)
+camera.position.set(5,5,-5);
 scene.add(camera)
 
 /////////////////////////////////////////////////////////////////////////
@@ -57,20 +62,76 @@ const controls = new OrbitControls(camera, renderer.domElement)
 
 /////////////////////////////////////////////////////////////////////////
 ///// SCENE LIGHTS
-const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
+const ambient = new THREE.AmbientLight(0xa0a0fc, 0.5)
 scene.add(ambient)
 
 const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96)
 sunLight.position.set(-69,44,14)
-scene.add(sunLight)
+sunLight.castShadow = true;
+sunLight.shadow.bias = 0.001;
+sunLight.shadow.mapSize.width = 1024*8;
+sunLight.shadow.mapSize.height = 1024*8;
+//scene.add(sunLight)
+
+let light = new THREE.SpotLight(0xfff8e8,0.5);
+light.position.set(5,10,7);
+light.castShadow = true;
+light.shadow.bias = 0.001;
+light.shadow.mapSize.width = 1024*8;
+light.shadow.mapSize.height = 1024*8;
+scene.add( light );
+
 
 /////////////////////////////////////////////////////////////////////////
 ///// LOADING GLB/GLTF MODEL FROM BLENDER
-loader.load('models/gltf/starter-scene.glb', function (gltf) {
-
-    scene.add(gltf.scene)
+loader.load('models/gltf/scene3.gltf', function (gltf) {
+    let model = gltf.scene.children[0];
+    console.log(model);
+    model.traverse(n => { if ( n.isMesh ) {
+      n.castShadow = true; 
+      n.receiveShadow = true;
+      if(n.material.map) n.material.map.anisotropy = 32; 
+    }});
+    scene.add(gltf.scene);
 })
 
+
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+    'models/fbx/CLEAN_ROOM.fbx',
+    (object) => {
+        // object.traverse(function (child) {
+        //     if ((child as THREE.Mesh).isMesh) {
+        //         // (child as THREE.Mesh).material = material
+        //         if ((child as THREE.Mesh).material) {
+        //             ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+        //         }
+        //     }
+        // })
+        // object.scale.set(.01, .01, .01)
+        object.position.set(0,1.70,0)
+        scene.add(object)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
+//////////////////////////////////////////////////////////////////////////
+///// Environment HDR
+const hdrTextureURL = new URL('../static/assets/images/rostock_laage_airport_2k.hdr',import.meta.url);
+const hdrLoader  = new RGBELoader();
+hdrLoader.load(hdrTextureURL, function(texture){
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  //scene.background = texture;
+  scene.environment = texture;
+  
+})
 /////////////////////////////////////////////////////////////////////////
 //// INTRO CAMERA ANIMATION USING TWEEN
 function introAnimation() {
@@ -89,18 +150,18 @@ function introAnimation() {
     })
 }
 
-introAnimation() // call intro animation on start
+//introAnimation() // call intro animation on start
 
 /////////////////////////////////////////////////////////////////////////
 //// DEFINE ORBIT CONTROLS LIMITS
 function setOrbitControlsLimits(){
     controls.enableDamping = true
     controls.dampingFactor = 0.04
-    controls.minDistance = 35
-    controls.maxDistance = 60
+    //controls.minDistance = 35
+    //controls.maxDistance = 60
     controls.enableRotate = true
     controls.enableZoom = true
-    controls.maxPolarAngle = Math.PI /2.5
+    //controls.maxPolarAngle = Math.PI /2.5
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -110,7 +171,7 @@ function rendeLoop() {
     TWEEN.update() // update animations
 
     controls.update() // update orbit controls
-
+   
     renderer.render(scene, camera) // render the scene using the camera
 
     requestAnimationFrame(rendeLoop) //loop the render function
